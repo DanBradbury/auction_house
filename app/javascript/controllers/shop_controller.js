15b93ebf719aca06b30fd5cc2bc2d25141dbf3a7
Dan Bradbury <dan.luckydaisy@gmail.com>
2024-07-04 07:42:45 -0700

    more bgs

--- app/javascript/controllers/shop_controller.js
+++ app/javascript/controllers/shop_controller.js
@@ -2,11 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
+    console.log("hello shop")
     var g = document.getElementById("game")
-    //g.classList.remove("worlds")
-    //g.classList.add("fights")
     g.classList = []
-    g.classList.add("game", "fights")
+    g.classList.add("game", "shop")
+
   }
 }
 
